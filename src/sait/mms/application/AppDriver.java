6fdf95e42f21ac09ee5ff77d4108c0724267ecef
Jed <jedidiah.belayneh@edu.sait.ca>
2026-02-02 11:03:22 -0700

    Starting code

---
+++ src/sait/mms/application/AppDriver.java
@@ -0,0 +1,13 @@
+package sait.mms.application;
+
+import sait.mms.manager.MovieManager;
+
+public class AppDriver {
+
+	public static void main(String[] args) {
+		new MovieManager();
+		//Finished/Don't touch
+
+	}
+
+}
---
+++ src/sait/mms/manager/MovieManager.java
@@ -0,0 +1,58 @@
+package sait.mms.manager;
+
+import java.util.*;//* means import all 
+import sait.mms.problemdomain.Movie;
+
+public class MovieManager {
+	
+	//attributes
+	private ArrayList<Movie> movies = new ArrayList<>();
+	private Scanner keyboard = new Scanner(System.in);
+	
+	public MovieManager() {
+		loadMovieList();
+		displayMenu();
+	}
+	
+	//loadMovieList
+	private void loadMovieList() {
+		// TODO Auto-generated method stub
+		//store movie in ArrayList
+	}	
+	
+	//displayMenu
+	private void displayMenu() {
+		//choice 1:
+		addMovie();
+		//choice 2:
+		generateMovieListInYear();
+		//choice 3:
+		generateRandomMovieList();
+		//choice 4:
+		saveMovieListToFile();
+		
+	}
+	//addMovie	
+	private void addMovie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//generateMovieListInYear	
+	private void generateMovieListInYear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//generateRandomMovieList	
+	private void generateRandomMovieList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//saveMovieListToFile
+	private void saveMovieListToFile() {
+		// TODO Auto-generated method stub
+		
+	}		
+}
